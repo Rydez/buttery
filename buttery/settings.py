276e2e0cb315ec50c5f553afa385e05d02b5d3e8
Ryan Melendez <rmelend1@kent.edu>
2018-03-11 13:33:42 -0400

    Fix something

--- buttery/settings.py
+++ buttery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5g_!^6!3qf^q!^ok107^1(#r%xafivav^a8z+jj4hk7nn@9n1+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,5 +138,5 @@ ALLOWED_HOSTS = [
 
 EMAIL_HOST = 'smtp.webfaction.com'
 EMAIL_HOST_USER = 'buttery'
-EMAIL_HOST_PASSWORD = 'Brandnewinvention4!'
+EMAIL_HOST_PASSWORD = ''
 SERVER_EMAIL = 'support@butteryaf.com'
